d73d8b7e4134d2ba500de7cbdbc7e4b6f0b021a1
rishith2903 <rishithkumar2903@gmail.com>
2025-09-22 04:01:42 +0530

    fix

--- src/main/java/com/medreserve/repository/RoleRepository.java
+++ src/main/java/com/medreserve/repository/RoleRepository.java
@@ -8,8 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
-    Optional<Role> findByName(String name);
+    Optional<Role> findByName(Role.RoleName name);
 
-    boolean existsByName(String name);
+    boolean existsByName(Role.RoleName name);
 }
-
