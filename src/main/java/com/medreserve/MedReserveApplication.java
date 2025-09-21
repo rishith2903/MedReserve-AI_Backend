8bc50adf7d955c0928180e8a8b70dfebe9a49b8f
Sumanth-2212 <k.saisumanthchowdary3313@gmail.com>
2025-09-21 19:30:58 +0530

    update

--- src/main/java/com/medreserve/MedReserveApplication.java
+++ src/main/java/com/medreserve/MedReserveApplication.java
@@ -2,10 +2,12 @@ package com.medreserve;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ComponentScan
 public class MedReserveApplication {
 
     public static void main(String[] args) {
